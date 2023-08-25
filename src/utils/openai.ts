78f6cc7a97f74ccd2c8e35ff44b6f83d1970d4b4
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-08-25 17:29:38 -0400

    perf: reuse openext client
    better performance

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -31,57 +31,44 @@ export async function createStreamChatCompletion(
     const chat = await db.chats.get(chatId);
     const settings = await db.settings.get("general");
     const model = chat?.modelUsed ?? settings?.openAiModel ?? config.defaultModel;
-    const type = settings?.openAiApiType ?? config.defaultType;
-    const auth = settings?.openAiApiAuth ?? config.defaultAuth;
-    const base = settings?.openAiApiBase ?? config.defaultBase;
-    const version = settings?.openAiApiVersion ?? config.defaultVersion;
+    const chatCompletionsUrl = settings?.openAiApiBase ? settings?.openAiApiBase + "/chat/completions" : undefined;
 
-    const client = getClient(apiKey, type, auth, base);
 
-    const stream = await client.chat.completions.create(
+    return OpenAIExt.streamClientChatCompletion(
         {
             model,
-            stream: true,
             messages,
         },
-
         {
-            headers: {
-                "Content-Type": "application/json",
-                ...(type === "custom" && auth === "api-key" && {"api-key": apiKey}),
-            },
-            query: {
-                ...(type === "custom" && {"api-version": version}),
+            apiKey: apiKey,
+            chatCompletionsUrl: chatCompletionsUrl,
+            handler: {
+                onContent(content, isFinal, stream) {
+                    setStreamContent(messageId, content, isFinal);
+                    if (isFinal) {
+                        setTotalTokens(chatId, content);
+                    }
+                },
+                onDone(stream) {
+                },
+                onError(error, stream) {
+                    console.error(error);
+                },
             },
         }
     );
-
-    db.messages.where({id: messageId}).modify((message) => {
-        message.content = "";
-    });
-    let lastPart = null;
-    for await (const part of stream) {
-        appendToContent(messageId, chatId, part.choices[0].delta.content!);
-        lastPart = part;
-    }
-    if (lastPart != null) {
-        db.chats.where({id: chatId}).modify((chat) => {
-            chat.modelUsed = lastPart!.model;
-        });
-    }
 }
 
-function appendToContent(
+function setStreamContent(
     messageId: string,
-    chatId: string,
     content: string,
+    isFinal: boolean
 ) {
-    if (content == null || content == "") {
-        return;
-    }
-    db.messages.where({id: messageId}).modify((message) => {
-        message.content += content;
-    });
+    content = isFinal ? content : content + "█";
+    db.messages.update(messageId, {content: content});
+}
+
+function setTotalTokens(chatId: string, content: string) {
     let total_tokens = encode(content).length;
     db.chats.where({id: chatId}).modify((chat) => {
         chat.totalTokens = (chat.totalTokens ?? 0) + total_tokens;
