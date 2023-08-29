e9a41adbe9d17bb2af72090a716316d0ea12f71a
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-08-28 23:05:14 -0400

    fix: be sure to tag that the chat isn't new when it got it's title

--- src/utils/chatUpdateTitle.ts
+++ src/utils/chatUpdateTitle.ts
@@ -9,7 +9,7 @@ export async function updateChatTitle(chat: Chat, apiKey: string) {
     const messages = await db.messages
         .where({chatId: chat.id})
         .sortBy("createdAt");
-    const createChatDescription = await createChatCompletion(apiKey, chat.id,[
+    const createChatDescription = await createChatCompletion(apiKey, chat.id, [
         ...(messages ?? []).map((message) => ({
             role: message.role,
             content: message.content,
@@ -29,6 +29,7 @@ export async function updateChatTitle(chat: Chat, apiKey: string) {
             chat.description = trim(description, "\"");
             chat.modelUsed = createChatDescription.model;
             chat.updatedAt = new Date();
+            chat.isNewChat = false;
         });
     }
 }
